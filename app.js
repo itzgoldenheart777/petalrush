ff207d2efcd5c58859c7cf8f14ea72ac6d8792f0
itz_nitin_317 <ns077751@gmail.com>
2026-02-16 03:16:45 +0530

    Professional UI and fixed login

--- app.js
+++ app.js
@@ -1,13 +1,74 @@
-
 const supabaseUrl =
 "https://lssjsgfppehhclxqulso.supabase.co";
 
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxzc2pzZ2ZwcGVoaGNseHF1bHNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzExOTAwNzksImV4cCI6MjA4Njc2NjA3OX0.nRq1iFBiOEyty0ALRmS45ARoso7BsB0ENOvttu7nvX0";
+const supabaseKey =
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxzc2pzZ2ZwcGVoaGNseHF1bHNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzExOTAwNzksImV4cCI6MjA4Njc2NjA3OX0.nRq1iFBiOEyty0ALRmS45ARoso7BsB0ENOvttu7nvX0";
 
-const client = supabase.createClient(
+const client =
+supabase.createClient(
 supabaseUrl,
 supabaseKey
 );
+
+
+// Signup
+async function signup(){
+
+const email =
+document.getElementById("email").value;
+
+const password =
+document.getElementById("password").value;
+
+const { error } =
+await client
+.from("users")
+.insert([{email,password}]);
+
+if(error){
+
+alert(error.message);
+
+}else{
+
+alert("Signup success");
+
+}
+
+}
+
+
+// Login
+async function login(){
+
+const email =
+document.getElementById("email").value;
+
+const password =
+document.getElementById("password").value;
+
+const { data,error } =
+await client
+.from("users")
+.select("*")
+.eq("email",email)
+.eq("password",password);
+
+if(data && data.length>0){
+
+localStorage.setItem("user",email);
+
+alert("Login success");
+
+loadOrders();
+
+}else{
+
+alert("Invalid login");
+
+}
+
+}
 
 
 // Load flowers
@@ -34,7 +95,7 @@ container.innerHTML+=`
 <p>₹${flower.price}</p>
 
 <button onclick="orderFlower('${flower.name}',${flower.price})">
-Order Now
+Order
 </button>
 
 </div>
@@ -42,57 +103,6 @@ Order Now
 `;
 
 });
-
-}
-
-
-// Signup
-async function signup(){
-
-const email =
-document.getElementById("email").value;
-
-const password =
-document.getElementById("password").value;
-
-await client
-.from("users")
-.insert([{email,password}]);
-
-alert("Signup success");
-
-}
-
-
-// Login
-async function login(){
-
-const email =
-document.getElementById("email").value;
-
-const password =
-document.getElementById("password").value;
-
-const { data } =
-await client
-.from("users")
-.select("*")
-.eq("email",email)
-.eq("password",password);
-
-if(data.length>0){
-
-localStorage.setItem("user",email);
-
-alert("Login success");
-
-loadOrders();
-
-}else{
-
-alert("Login failed");
-
-}
 
 }
 
@@ -105,7 +115,7 @@ localStorage.getItem("user");
 
 if(!email){
 
-alert("Please login");
+alert("Login first");
 
 return;
 
